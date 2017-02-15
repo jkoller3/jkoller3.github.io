94ae3bbc960fe071e4063b19aafc02abddd1e09c
jkoller90 <jmk4411@gmail.com>
2017-02-15 01:18:47 -0500

    added some comments to js, removed unnecessary files

--- assets/modal_switch.js
+++ assets/modal_switch.js
@@ -1,5 +1,5 @@
-var incrementScoreVar = 0;
-var wallpaper;
+var incrementScoreVar = 0; //reference for hooking up backend
+var wallpaper; // will be used in css image-background property for the user to download 
 //on load, the z-index of the image will be below the overlay. the confirm dialog will bring the image up to be available to a contextmenu in a taphold event 
 $("#tobedownloaded").click(function (e) {
 	var imageZshift = 'position:fixed;top:0px;left:0px;width:100%;height:100%;z-index:1;';
@@ -16,12 +16,14 @@ $("#tobedownloaded").click(function (e) {
 	//		}
 	//waiting on backend implementation here 
 });
+
+//understanding the instructions, user presses "next":
 $("#introNextBTN").click(function (e) {
 	$("#intro").css("visibility", "hidden");
 	$("#gameModal").css("visibility", "visible");
 	$("li a.DLimg").css("visibility", "visible");
-//	$(".bg").css("visibility", "visible");
 });
+//download link on selected carousel image
 $(".DLimg").click(function (event) {
 	wallpaper = $(this).parent().find("img").attr("src");
 	$("#gameModal").css("visibility", "hidden"); //hide modal to show new modal
@@ -31,25 +33,27 @@ $(".DLimg").click(function (event) {
 	$("li a.DLimg").css("visibility", "hidden");
 //	$(".bg").css("visibility", "hidden");
 });
+//closing carousel:
 $("#carousel-close").click(function () {
 	$("#gameModal").css("visibility", "hidden");
 	$("#intro").css("visibility", "visible");
 	$("li a.DLimg").css("visibility", "hidden");
 });
+//closing instructions page:
 $("#instructions-close").click(function () {
 	$("#instructions").css("visibility", "hidden");
 	$("#gameModal").css("visibility", "visible");
 	$("li a.DLimg").css("visibility", "visible");
-//	$(".bg").css("visibility", "visible");
 	$("section").css("overflow", "hidden");
 });
+//pressing "got it" on instructions page:
 $("#gotitBTN").click(function () {
 	$("#instructions").css("visibility", "hidden");
 	$("#wallpaperModal").attr("style", "visibility:visible; padding-top: 73px; padding-bottom: 54px;");
 	$("#tobedownloaded").attr("src", wallpaper);
 	incrementScoreVar++; //<- This is just for readibility. This is when the user agrees to download the image; we just trust that they do 
 });
-var carouselpage;
+//closing carousel game, going back to first introduction page 
 $("#game-close").click(function () {
 	$("#wallpaperModal").attr("style", "visibility:hidden; padding-top: 73px; padding-bottom: 54px;");
 	$("#gameModal").css("visibility", "visible");

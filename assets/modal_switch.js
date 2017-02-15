3d5d53a2e20a6577710f5511bf9a9ca3ded36d79
jkoller90 <jmk4411@gmail.com>
2017-02-14 22:36:14 -0500

    working on new images

--- assets/modal_switch.js
+++ assets/modal_switch.js
@@ -1,13 +1,5 @@
 var incrementScoreVar = 0;
 var wallpaper;
-$(".DLimg").click(function (event) {
-    wallpaper = $(this).parent().find("img").attr("src");
-    $("#gameModal").css("visibility", "hidden"); //hide modal to show new modal
-    $("#wallpaperModal").attr("style", "visibility:show; padding-top: 73px; padding-bottom: 54px;");
-    $("#tobedownloaded").attr("src", wallpaper);
-    incrementScoreVar++; //<- just for readibility.
-    alert("Tap and hold on the image to download the wallpaper!");
-});
 //on load, the z-index of the image will be below the overlay. the confirm dialog will bring the image up to be available to a contextmenu in a taphold event 
 $("#tobedownloaded").click(function (e) {
     var imageZshift = 'position:fixed;top:0px;left:0px;width:100%;height:100%;z-index:1;';
@@ -27,10 +19,20 @@ $("#introNextBTN").click(function (e) {
     $("#intro").css("visibility", "hidden");
     $("#gameModal").css("visibility", "visible");
 });
+$(".DLimg").click(function (event) {
+    wallpaper = $(this).parent().find("img").attr("src");
+		$("#instructions").css("visibility","show");
+    $("#gameModal").css("visibility", "hidden"); //hide modal to show new modal
+		$(".modal-iframe").css("overflow","scroll"); //allows scrolling. should be adjusted according to current view.
+	
+//    $("#wallpaperModal").attr("style", "visibility:show; padding-top: 73px; padding-bottom: 54px;");
+//    $("#tobedownloaded").attr("src", wallpaper);
+    incrementScoreVar++; //<- just for readibility.
+//    alert("Tap and hold on the image to download the wallpaper!");
+});
 $("#carousel-close").click(function () {
     $("#gameModal").css("visibility", "hidden");
 		$("#intro").css("visibility", "visible");
-
 });
 var carouselpage;
 $("#game-close").click(function () {
